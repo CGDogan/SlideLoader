7e8b1c6fb84bdca17d9d479eb9d655517388c487
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-14 20:00:03 +0100

    Update SlideServer.py

--- SlideServer.py
+++ SlideServer.py
@@ -82,6 +82,9 @@ def getThumbnail(filename, size=50):
         thumb = slide.get_thumbnail((size, size))
         buffer = BytesIO()
         thumb.save(buffer, format="PNG")
+        print("Buffer len: ")
+        print(len(buffer))
+        print(thumb)
         data = 'data:image/png;base64,' + str(base64.b64encode(buffer.getvalue()))[2:-1]
         return {"slide": data, "size": size}
     except BaseException as e:
