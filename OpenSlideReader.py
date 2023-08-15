25fe78e78e35ce72fa1c7dcba18207a1a9bad1c1
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-15 20:11:38 +0100

    Update OpenSlideReader.py

--- OpenSlideReader.py
+++ OpenSlideReader.py
@@ -7,26 +7,26 @@ class OpenSlideReader(ImageReader.ImageReader):
 
     # Pick the reader
     def __init__(self, imagepath):
-        self.reader = openslide.OpenSlide(imagepath)
+        self._reader = openslide.OpenSlide(imagepath)
     
     @property
     def level_count(self):
-        return self.reader.level_count
+        return self._reader.level_count
 
     @property
     def dimensions(self):
-        return self.reader.dimensions
+        return self._reader.dimensions
 
     @property
     def level_dimensions(self):
-        return self.reader.level_dimensions
+        return self._reader.level_dimensions
     
     @property
     def associated_images(self):
-        return self.reader.associated_images
+        return self._reader.associated_images
 
     def read_region(self, location, level, size):
-        return self.reader.read_region(location, level, size)
+        return self._reader.read_region(location, level, size)
     
     def get_thumbnail(self, max_size):
-        return self.reader.get_thumbnail(max_size)
+        return self._reader.get_thumbnail(max_size)
