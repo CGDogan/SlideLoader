57f5f71ea380eb3323365df4b5446ea848bf7e4c
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-15 06:33:49 +0100

    print

--- SlideServer.py
+++ SlideServer.py
@@ -78,6 +78,7 @@ def getThumbnail(filename, size=50):
     if not os.path.isfile(filepath):
         return {"error": "No such file"}
     try:
+        print("getThumbnail called")
         slide = ImageReader.ImageReader(filepath)
         thumb = slide.get_thumbnail((size, size))
         buffer = BytesIO()
@@ -86,7 +87,7 @@ def getThumbnail(filename, size=50):
         print(len(buffer))
         print(thumb)
 
-        
+
         data = 'data:image/png;base64,' + str(base64.b64encode(buffer.getvalue()))[2:-1]
         return {"slide": data, "size": size}
     except BaseException as e:
