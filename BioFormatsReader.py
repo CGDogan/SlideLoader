e8e4fa3956e226dcae08895c01e4732767d84b58
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-24 10:59:37 +0100

    Revert "revertme2: use submodule?"
    This reverts commit ed47e18dfd476ff83096a50a49d8b4fb7e59119f.

--- BioFormatsReader.py
+++ BioFormatsReader.py
@@ -2,7 +2,7 @@ import image_reader
 import dev_utils
 import ome_types
 from file_extensions import BIOFORMATS_EXTENSIONS
-import BFBridge.python as bfbridge
+import bfbridge.python as bfbridge
 
 
 jvm = bfbridge.BFBridgeVM()
