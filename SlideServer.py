f264d07db81acad0827ba6103bd2e58b5ca8106d
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-09-16 09:23:22 +0100

    Update SlideServer.py

--- SlideServer.py
+++ SlideServer.py
@@ -75,6 +75,8 @@ def secure_relative_path(filename):
         raise ValueError("Filepath contains '//' which is forbidden")
     if ".." in filename:
         raise ValueError("Filepath contains '..' which is forbidden")
+    if filename[0] == '.':
+        raise ValueError("Filepath starts with '.' (or is '.') which is forbidden")
     level_names = filename.split(os.sep)
     filename = ""
     for name in level_names:
